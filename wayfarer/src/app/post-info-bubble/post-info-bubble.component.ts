8798c9cb2863e3c24c052f38b95d9b540c7923e2
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-13 17:52:44 -0400

    Add basic layout for 'post-info-bubble'

--- wayfarer/src/app/post-info-bubble/post-info-bubble.component.ts
+++ wayfarer/src/app/post-info-bubble/post-info-bubble.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-info-bubble.component.css']
 })
 export class PostInfoBubbleComponent {
-
+  postTitle: string = "San Francisco";
+  cityImage: string = "https://assets.editorial.aetnd.com/uploads/2015/02/topic-golden-gate-bridge-gettyimages-177770941.jpg?width=3840&height=1920&crop=3840%3A1920%2Csmart&quality=75";
+  postSummary: string = "SLAM YOUR HANDS"
 }
