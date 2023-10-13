b9c37a6f5bf8d1712dc4abd6c482dfd54a462fc5
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-13 16:15:48 -0400

    Link Home page

--- wayfarer/src/app/app.module.ts
+++ wayfarer/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { LandingComponent } from './landing/landing.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { TopicComponent } from './topic/topic.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    TopicComponent
+    TopicComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
