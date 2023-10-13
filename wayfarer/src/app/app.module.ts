6acc2f44bc0ee0e19984e576546ba1179f50ae22
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-13 11:58:49 -0400

    add angular and bootstrap

---
+++ wayfarer/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
