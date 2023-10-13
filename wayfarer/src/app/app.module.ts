141d9960d6e36fdce9a9ac721a0daf3f6d95919f
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-13 12:06:09 -0400

    Add cariousel component

--- wayfarer/src/app/app.module.ts
+++ wayfarer/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
