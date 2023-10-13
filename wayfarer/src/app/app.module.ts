42ec967df177a06c3d3fb87c6008893623242abb
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-13 12:06:42 -0400

    Add topic component

--- wayfarer/src/app/app.module.ts
+++ wayfarer/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { TopicComponent } from './topic/topic.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    CarouselComponent
+    CarouselComponent,
+    TopicComponent
   ],
   imports: [
     BrowserModule
