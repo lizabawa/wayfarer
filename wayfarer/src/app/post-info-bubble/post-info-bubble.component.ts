1eebe70848435ef6c8197fd9dd61d5c6ddc74ca7
GabrielleHandy <gehandy123@gmail.com>
2023-10-14 01:29:06 -0400

    Add news data api on city change and minor css

--- wayfarer/src/app/post-info-bubble/post-info-bubble.component.ts
+++ wayfarer/src/app/post-info-bubble/post-info-bubble.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component} from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-info-bubble',
@@ -9,4 +11,32 @@ export class PostInfoBubbleComponent {
   postTitle: string = "San Francisco";
   cityImage: string = "https://assets.editorial.aetnd.com/uploads/2015/02/topic-golden-gate-bridge-gettyimages-177770941.jpg?width=3840&height=1920&crop=3840%3A1920%2Csmart&quality=75";
   postSummary: string = "SLAM YOUR HANDS"
+
+  posts: any= [];
+
+  
+  constructor(private http: HttpClient, private router: Router){}
+
+  ngOnInit() {
+    this.router.events.subscribe((evt: any) => {
+      
+      if (evt instanceof NavigationEnd) {
+        if(evt.url.length> 6){
+
+          this.http.get(`https://newsdata.io/api/1/news?apikey=pub_31144c11ee91a6edd0ba96fafd5ff6ed7b7b4&q=${evt.url.slice(6)}`)
+          .subscribe(res => {
+              (res as any).results.forEach((post: any) => {
+                if(this.posts.length < 3){
+                  this.posts.push(post);
+                }
+                
+
+        })
+        
+      })
+
+  }}})
+
+  }
+
 }
